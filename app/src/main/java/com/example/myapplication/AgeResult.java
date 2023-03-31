a3fd88a58180c9ee808fe2e96082a103d156d8b5
marufnahid <maruf155714@outlook.com>
2023-03-31 07:25:56 +0600

    Fully worked

--- app/src/main/java/com/example/myapplication/AgeResult.java
+++ app/src/main/java/com/example/myapplication/AgeResult.java
@@ -3,10 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
-
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 public class AgeResult extends AppCompatActivity {
 
@@ -22,25 +23,23 @@ public class AgeResult extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
 
-
-        if (bundle != null){
-            String value = bundle.getString("dob");
-
-            LocalDate today = null;
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                today = LocalDate.now();
-            }
-            LocalDate birthday = null;
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                birthday = LocalDate.of(1987, 9, 24);
-            }
-
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                Period period = Period.between(birthday, today);
-            }
+//        Log.println(Log.ASSERT, "helloworld", String.valueOf(bundle));
 
 
-            resultAge.setText(value);
+        if (bundle != null) {
+            String dob = bundle.getString("dob");
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/M/yyyy");
+            LocalDate birthDate = LocalDate.parse(dob, formatter);
+
+
+            LocalDate currentDate = LocalDate.now();
+            Period period = Period.between(birthDate, currentDate);
+            int days = period.getDays();
+            int months = period.getMonths();
+            int year = period.getYears();
+
+            resultAge.setText( "Your age is " + String.valueOf(year) + " years " + String.valueOf(months) + " months "  + days +" days.");
 
         }
     }
